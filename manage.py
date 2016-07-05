35d705a2a12799a60d642f486a46c62915aaa8a7
huangleping <791616265@qq.com>
2016-07-05 15:57:09 +0800

    20160705

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 # 脚本，初始化数据，任务
 from nowstagram import app, db
 from flask_script import Manager
+from sqlalchemy import or_, and_  # 复杂的操作查询
 from nowstagram.models import User, Image, Comment
 import random
 
@@ -17,12 +18,37 @@ def init_database():
     db.drop_all()  # 删除表
     db.create_all()  # 创建表
     for i in range(0, 100):
-        db.session.add(User('User' + str(i), 'a' + str(i)))
+        db.session.add(User('User' + str(i + 1), 'a' + str(i)))
         for j in range(0, 3):
             db.session.add(Image(get_image_url(), i + 1))
             for k in range(0, 3):
-                db.session.add(Comment('This is a comment' + str(k),1+3*i+j,i+1))
+                db.session.add(Comment('This is a comment' + str(k), 1 + 3 * i + j, i + 1))
     db.session.commit()
+    # 查询
+    print 1, User.query.all()  # 查询所有
+    print 2, User.query.get(3)  # 查询第三个用户
+    print 3, User.query.filter_by(id=5).first()
+
+    # order_by是排序，desc降序，asc升序 offset偏移（从99开始）,limit（2）只找两个，all打印出所有内容
+    # 数据库语句为 select * from user order by id desc limit 1,2
+    print 4, User.query.order_by(User.id.desc()).offset(1).limit(2).all()
+
+    print 5, User.query.filter(User.username.endswith('0')).limit(3).all()  # 末尾是0的找出来
+    print 6, User.query.filter(or_(User.id == 88, User.id == 99)).all()
+    # 没有all语句，可以直接打印sql语句
+    print 7, User.query.filter(and_(User.id > 88, User.id < 93)).all()
+    print 8, User.query.filter(and_(User.id > 88, User.id < 93)).first_or_404()
+
+    # 分页
+    print 9, User.query.paginate(page=1, per_page=10).items
+    # 逆序打印print 9, User.query.order_by(User.id.desc()).paginate(page=1, per_page=10).items
+
+    #一对多，打印user关联内容
+    user = User.query.get(1)
+    print 10, user.images.all()
+
+    image = Image.query.get(1)
+    print 11,image.user
 
 
 if __name__ == '__main__':
--- nowstagram/models.py
+++ nowstagram/models.py
@@ -9,16 +9,17 @@ class Comment(db.Model):
     content = db.Column(db.String(1024))  # 内容
     image_id = db.Column(db.Integer, db.ForeignKey('image.id'))  # 外键
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 外键
-    status = db.Column(db.Integer,default=0)  # 0正常，1表示被删除
+    status = db.Column(db.Integer, default=0)  # 0正常，1表示被删除
     user = db.relationship('User')
 
-    def __init__(self, content,image_id,user_id):
+    def __init__(self, content, image_id, user_id):
         self.content = content
         self.image_id = image_id
         self.user_id = user_id
 
     def __repr__(self):
         return '<Comment %d %s>' % (self.id, self.content)
+
 
 """图片地址为：http://images.nowcoder.com/head/99m.png"""
 
@@ -41,11 +42,21 @@ class Image(db.Model):
 
 
 class User(db.Model):
+    # __tablename__ = 'xuser' 增加这句，需要把之前上面关联的ForeignKey和user有关的都改成xuser
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # id是数据库中的一列，主键，自增长
     username = db.Column(db.String(80), unique=True)  # 定义为字符串类型，unique保证唯一
     password = db.Column(db.String(32))  # 定义为字符串类型
     head_url = db.Column(db.String(256))  # 头像
-    images = db.relationship('Image')  # 关联表
+    #images = db.relationship('Image')  # 关联表
+
+    """
+    image = Image.query.get(1)
+    print 11,image.user
+
+    增加backref = user,内置一个变量
+    """
+    images = db.relationship('Image',backref = 'user',lazy = 'dynamic')#关联属性
+
 
     def __init__(self, username, password):
         self.username = username
